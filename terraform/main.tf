c8816912bf2afe4339c8af3facbb2f3340470154
Terrence Ryan <tpryan@google.com>
2022-09-30 09:45:08 -0700

    fix: removing special characters that I suspect was screwing up schema loading.

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@
 resource "random_password" "password" {
   length           = 16
   special          = true
-  override_special = "!#$%&*()-_=+[]{}<>:?"
+  override_special = "!#$%*-_=+<>:?"
 }
 
 
